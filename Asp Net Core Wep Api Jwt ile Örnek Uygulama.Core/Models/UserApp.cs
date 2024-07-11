b087131e6f4acf79bf0b4ab92235325efbcf2dd8
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-11 23:56:20 +0300

    UserApp entity'i için City alanı eklendi.IdentityUser'ten miras aldı.

---
+++ Asp Net Core Wep Api Jwt ile Örnek Uygulama.Core/Models/UserApp.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Models
+{
+    public class UserApp:IdentityUser
+    {
+        public string City { get; set; }
+    }
+}
