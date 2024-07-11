44c51989a5e3e0f06786a32d4a84121d2d9e042b
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-11 23:48:55 +0300

    Product entity oluşturuldu

---
+++ Asp Net Core Wep Api Jwt ile Örnek Uygulama.Core/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Stock { get; set; }
+        public string UserId { get; set; }
+    }
+}
