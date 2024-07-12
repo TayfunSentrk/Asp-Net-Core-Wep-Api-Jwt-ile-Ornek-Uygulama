b28e3f76157b41ee73d4bf6f0d590b99bee84103
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-12 10:51:28 +0300

    Token Dto ve Client Dto oluşturuldu

---
+++ Asp Net Core Wep Api Jwt ile Örnek Uygulama.Core/Dtos/ClientTokenDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Dtos
+{
+    public class ClientTokenDto
+    { 
+        public string AccessToken { get; set; }        //sadece clientler için
+          
+        public DateTime AccessTokenExpiration { get; set; }
+    }
+}
---
+++ Asp Net Core Wep Api Jwt ile Örnek Uygulama.Core/Dtos/TokenDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Dtos
+{
+    public class TokenDto
+    {
+        public string AccessToken { get; set; }
+
+        public DateTime AccessTokenExpiration { get; set; }
+        public string RefreshToken { get; set; } //bu dataları veri tabanına kaydeciem
+        public DateTime RefreshTokenExpiration { get; set; } //bu dataları veri tabanına kaydeciem
+    }
+}
