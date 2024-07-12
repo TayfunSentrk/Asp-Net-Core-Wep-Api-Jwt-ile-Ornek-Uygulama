368a2f111fa6ceac62f3e216cd43af765d5d7c7b
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-12 11:10:12 +0300

    IToken Service ve client nesnesi olıuşturuldu

---
+++ Asp Net Core Wep Api Jwt ile Örnek Uygulama.Core/Configration/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Configration
+{
+    public class Client       
+                                            //bunlar mobil uygulaması olabilir herhangi entity yada dto değil 
+    {
+        public string Id { get; set; }  
+
+
+        public int Secred { get; set; } //password gibi
+
+        //wwww.myapi1.com gibi bunlara erişim sağlayabilir
+        public List<string> Audiences { get; set; } // hangi apilere erişim sağlayacak onu belirleyiyoruz
+    }
+}
---
+++ Asp Net Core Wep Api Jwt ile Örnek Uygulama.Core/Services/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Configration;
+using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Dtos;
+using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp user); //user vererek tokenın kime ait olduunu belirliyoruz
+        ClientTokenDto CreateTokenByClient(Client client)
+    }
+}
