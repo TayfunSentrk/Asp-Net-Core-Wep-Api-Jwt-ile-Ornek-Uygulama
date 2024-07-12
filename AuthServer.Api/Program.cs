554351f9bb366ed6de264da6d3417b9c986b4bc6
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-12 23:05:36 +0300

    Servisler Eklendi.Context yolu belirlendi.İdentity ile ilgili yapılandırmalar yapıldı.

--- AuthServer.Api/Program.cs
+++ AuthServer.Api/Program.cs
@@ -1,4 +1,13 @@
 using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Configration;
+using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Models;
+using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Repositories;
+using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Services;
+using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.UnitOfWork;
+using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Data;
+using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Data.Repositories;
+using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Service.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using SharedLibrary.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,8 +20,29 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.Configure<CustomTokenOptions>(builder.Configuration.GetSection("TokenOptions"));//CustomTokensOptions'dll olarak istediðim yerde geçebilirim.Ýlgili datalarý section yapýsýndan alýcak
 builder.Services.Configure<List<Client>>(builder.Configuration.GetSection("Clients"));//bu client herhangi bir dll constructor'da eriþmek için
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+//generic olduðu için typeof kullandýk
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
-var app = builder.Build();
+//Generic yapý kullandýk fakat iki tane entity alacaðý için virgül koyduk
+builder.Services.AddScoped(typeof(IServiceGeneric<,>), typeof(GenericService<,>));
+builder.Services.AddScoped<IUnitofWork,UnitOfWork>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"), sqlOptions =>
+    {
+        sqlOptions.MigrationsAssembly("Asp Net Core Wep Api Jwt ile Örnek Uygulama.Data"); //burda migration assembly'data oluþturdum çünkü api assembly kirletmesin
+    });
+});
+
+builder.Services.AddIdentity<UserApp, IdentityRole>(options =>
+{
+    options.User.RequireUniqueEmail = true; //her bir mailin unique olmasýný saðladým
+    options.Password.RequireNonAlphanumeric = false; // * ? gibi ifadeleri zorunlu olmasýný engelledim
+}).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders(); //þifre sýfýrlamada token üretmek için yaptým
+var app = builder.Build(); 
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
