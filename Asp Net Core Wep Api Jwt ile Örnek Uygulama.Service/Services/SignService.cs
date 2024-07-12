8f975fdcc0092f57b08c04a13e1c0928f91a2dd9
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-12 16:18:57 +0300

    SignService  Oluşturuldu.Security Key methodu yazıldı.AuthServer.Api'de TokenOptions ile ilgili ayarlamalar yapıldı

---
+++ Asp Net Core Wep Api Jwt ile Örnek Uygulama.Service/Services/SignService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Service.Services
+{
+    public static class SignService
+    {
+
+        /// <summary>
+
+                ///Simetrik key imza ve doğrulama aynı string tipine ait
+                ///Public key ile private key aynı
+        /// </summary>
+
+        /// <returns>Bir security key vererek SymmetricSecurityKey dönüyoruz </returns>
+
+        public static SecurityKey GetSymmetricSecurityKey(string securityKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));   
+        }
+    }
+}
