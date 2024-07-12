045af2df2e7be7721c8b4fbb067160fdb553a881
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-12 11:48:03 +0300

    IUnitofWork interface oluşturuldu.Asekron ve asekron olmayan save işlemleri oluşturuldu

---
+++ Asp Net Core Wep Api Jwt ile Örnek Uygulama.Core/UnitOfWork/IUnitofWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.UnitOfWork
+{
+    public interface IUnitofWork
+    {
+        /// <summary>
+        /// Burda unitofwork kullanmamın sebebi birbiri ie ilişiki olan verilerde bir hata olduğundan diğerinin veritabınına yazılması .Roll Back yapılması
+        /// </summary>
+       
+
+        Task CommitAsync();
+        void Commit();
+    }
+}
