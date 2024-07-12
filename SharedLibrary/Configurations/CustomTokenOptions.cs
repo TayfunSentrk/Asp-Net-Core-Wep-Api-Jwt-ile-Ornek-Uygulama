e661928332081622f195fa280853174d2784d3d7
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-12 16:29:17 +0300

    Custom TokenOptions sınıfı oluşturuldu.Program cs'de dll olarak geçildi.İlgili datalar appsetting json tarafından doldurulcak

---
+++ SharedLibrary/Configurations/CustomTokenOptions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Configurations
+{
+    public class CustomTokenOptions
+    {
+
+        /// <summary>
+        /// Appsettin json'a tanımladığımız yerlerle aynı yapıları kullandım
+        /// Burda Auidence Kısmında hangi apile're istek yapılır 
+        /// Issuer token dağıtan adres
+        /// Access Token'ın Süresi
+        /// Refresh Token'ın Süresi
+        /// Security Key
+        /// </summary>
+
+        public List<string> Audiences { get; set; }
+        public string Issuer { get; set; }
+        public int AccessTokenExpiration { get; set; }
+        public int RefreshTokenExpiration { get; set; }
+        public string SecurityKey { get; set; }
+    }
+}
