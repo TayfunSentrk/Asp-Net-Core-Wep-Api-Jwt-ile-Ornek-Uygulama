7a663c1e294a7af16e2a94cbb9f9803718b586e2
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-13 00:23:57 +0300

    User controller kısmında user oluşturma ve user login gibi işlemler için gerekli actionlar yazıldı.

---
+++ AuthServer.Api/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Dtos;
+using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthServer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : BaseController
+    {
+        private readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        /// <summary>
+        /// clientLoginDto parametresine göre eğer başarılı ise Başarılı olup olmamasını döner
+        /// </summary>
+        /// <param name="clientLoginDto">Token oluşturmak nesne ilgili bilgileri içeren veri transfer nesnesi.</param>
+        /// <returns>Asenkron işlemi temsil eden bir görev. Görev sonucunda başarılı olup olmamasını döner.clientLoginDto ait veriler ile token oluşturulır.</returns>
+        ///
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserDto createUserDto)
+        {
+
+            return ActionResultInstance(await userService.CreateUserAsync(createUserDto));
+        }
+
+        /// <summary>
+        /// Identity'den gelen httpcontent ile ilgili isme göre user elde ettik
+        /// 
+        /// </summary>
+      
+        /// <returns>Asenkron işlemi temsil eden bir görev. Görev sonucunda başarılı olup olmamasını döner..</returns>
+        ///
+
+        [Authorize]
+        [HttpGet]
+
+        public async Task<IActionResult> GetUser()
+        {
+            return ActionResultInstance(await userService.GetUserByName(HttpContext.User.Identity.Name));
+        }
+
+
+    }
+}
