790a21f0b6af96186df365fe0855e5514d509728
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-12 13:13:41 +0300

    Dto Mapper oluşturuldu.Object mapper oluşturularak ne zaman çağırıldığı zaman memory'e yüklencek

---
+++ Asp Net Core Wep Api Jwt ile Örnek Uygulama.Service/DtoMapper.cs
@@ -0,0 +1,24 @@
+﻿using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Dtos;
+using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Service
+{
+    public class DtoMapper:Profile
+    {
+        /// <summary>
+        /// Burda product ve useraPP için mapping işlemleri yaptık.Burda tersine de olabilceği için reverve map yaptım.
+        /// </summary>
+        public DtoMapper()
+        {
+           
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto,UserApp>().ReverseMap();   
+        }
+    }
+}
---
+++ Asp Net Core Wep Api Jwt ile Örnek Uygulama.Service/ObjectMapper.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Service
+{
+    public static  class ObjectMapper
+    {
+        /// <summary>
+        /// Burda lazy yaptım çünkü uygulamada ne zaman çağırılırsa ozaman memory alınsın
+        /// </summary>
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DtoMapper>();
+            });
+            return config.CreateMapper();
+        });
+
+
+        public static IMapper Mapper => lazy.Value;  //burda set yapmıyorum sadece get' alıyorum ben object.mapper çağırdığımız zaman bu memory'e kayıt olucak
+    }
+}
