339df544cf417204677141684d866b7555d5444d
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-12 17:24:27 +0300

    Token Servis'te client'ler için token oluşturuldu

--- Asp Net Core Wep Api Jwt ile Örnek Uygulama.Service/Services/TokenService.cs
+++ Asp Net Core Wep Api Jwt ile Örnek Uygulama.Service/Services/TokenService.cs
@@ -4,6 +4,7 @@ using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Models;
 using Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Core.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using SharedLibrary.Configurations;
 using System;
 using System.Collections.Generic;
@@ -63,14 +64,100 @@ namespace Asp_Net_Core_Wep_Api_Jwt_ile_Örnek_Uygulama.Service.Services
             return userList;
 
         }
+
+        /// <summary>
+        /// Üyelik sistemi gerektirmeyen clientlar için Claim Listesi Oluşturuldu
+        /// </summary>
+        /// 
+        /// <returns>List tipinde token'ın payloadında claimsleri döner.</returns>
+
+        public IEnumerable<Claim> GetClaimsByClient(Client client)
+        {
+            var claims = new List<Claim>();
+            claims.AddRange(client.Audiences.Select(x => new Claim(JwtRegisteredClaimNames.Aud, x)));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+           claims.Add(new Claim(JwtRegisteredClaimNames.Sub, client.Id.ToString()));    //öznesi kimin için onun için 
+
+            return claims;
+
+        }
+
+        /// <summary>
+        /// İlk olarak bu token  hangi user'a ait  olucak onu parametre'de belirttim
+        /// Burda Access Token ve Refresh Token süreleri customtoken'dan geliyor
+        /// security key simetrik olarak oluşturuk ve ilgili security key custom token'dan aldık
+        /// SigningCredentials ile imzalama algoritması oluşturuldu
+        /// jwtSecurityToken token için gerekli özellikler belirlendi
+        /// JwtSecurityTokenHandler.writetoken methodu ile token oluşturuldu
+        /// Oluşturulan token tokendto'ya çevrildi
+        /// </summary>
+        /// 
+        /// <returns>Token Dto döner.</returns>
+
         public TokenDto CreateToken(UserApp user)
         {
-            throw new NotImplementedException();
+            var accessTokenExpiration = DateTime.Now.AddMinutes(customToken.AccessTokenExpiration); //tokenun ömr
+            var refreshTokenExpiration = DateTime.Now.AddMinutes(customToken.RefreshTokenExpiration);
+
+            var securityKey = SignService.GetSymmetricSecurityKey(customToken.SecurityKey);//tokenu imzalicak key
+
+            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);//Algoritma ile imza yapıyı oluşturduk
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(issuer: customToken.Issuer, expires: accessTokenExpiration,
+                notBefore: DateTime.Now, claims: GetClaims(user, customToken.Audiences), signingCredentials: signingCredentials
+                );
+
+            var handler = new JwtSecurityTokenHandler();
+            
+            var token=handler.WriteToken(jwtSecurityToken);
+            var tokenDto = new TokenDto()
+            {
+                AccessToken = token,
+                RefreshToken = CreateRefreshToken(),
+                AccessTokenExpiration = accessTokenExpiration,
+                RefreshTokenExpiration = refreshTokenExpiration
+            };
+
+            return tokenDto;
         }
+
+
+        /// <summary>
+        /// İlk olarak bu token  hangi client'a ait  olucak onu parametre'de belirttim
+        /// Burda Access Token customtoken'dan geliyor
+        /// security key simetrik olarak oluşturuk ve ilgili security key custom token'dan aldık
+        /// SigningCredentials ile imzalama algoritması oluşturuldu
+        /// jwtSecurityToken token için gerekli özellikler belirlendi
+        /// JwtSecurityTokenHandler.writetoken methodu ile token oluşturuldu
+        /// Oluşturulan token ClientTokenDto'ya çevrildi
+        /// </summary>
+        /// 
+        /// <returns>ClientTokenDto döner.</returns>
+
 
         public ClientTokenDto CreateTokenByClient(Client client)
         {
-            throw new NotImplementedException();
+            var accessTokenExpiration = DateTime.Now.AddMinutes(customToken.AccessTokenExpiration); //tokenun ömr
+         
+
+            var securityKey = SignService.GetSymmetricSecurityKey(customToken.SecurityKey);//tokenu imzalicak key
+
+            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);//Algoritma ile imza yapıyı oluşturduk
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(issuer: customToken.Issuer, expires: accessTokenExpiration,
+                notBefore: DateTime.Now, claims:GetClaimsByClient(client), signingCredentials: signingCredentials
+                );
+
+            var handler = new JwtSecurityTokenHandler();
+
+            var token = handler.WriteToken(jwtSecurityToken);
+            var clientTokenDto = new ClientTokenDto()
+            {
+                AccessToken = token,
+                AccessTokenExpiration = accessTokenExpiration,
+               
+            };
+
+            return clientTokenDto;
+
         }
     }
 }
