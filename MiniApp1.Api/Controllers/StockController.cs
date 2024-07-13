8838949b398bedde1ba7a121e1eeb6c6051881b8
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-13 14:16:17 +0300

    AuthApi dışındaki diğer api'ler için token durumu kontrol edildi

---
+++ MiniApp1.Api/Controllers/StockController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MiniApp1.Api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStock()
+        {
+            var userName = HttpContext.User.Identity.Name;
+            var userIdClaim = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier); //id yi almak içim
+
+            return Ok($"UserName:{userName}-UserId:{userIdClaim.Value}");
+        }
+    }
+}
