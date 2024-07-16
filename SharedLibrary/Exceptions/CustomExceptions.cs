779979a7c3fe8116a5bb4483cc9b7181470b86d4
TayfunSentrk <tayfun.senturk.yahoo.com>
2024-07-16 11:00:29 +0300

    CustomExpection Sınıfı oluşturuldu.Hataları global olarak yönetmek için

---
+++ SharedLibrary/Exceptions/CustomExceptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Exceptions
+{
+    public class CustomExceptions : Exception
+    {
+        public CustomExceptions():base()
+        {
+        }
+
+
+        public CustomExceptions(string message):base(message)
+        {
+            
+        }
+
+        public CustomExceptions(string message,Exception innerException):base(message, innerException) 
+        {
+            
+        }
+    }
+}
